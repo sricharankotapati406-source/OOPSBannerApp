bdf74cb647ef7e7034e0ac7ec04fa88c93bc2f54
sri charan <sricharankotapati406@gmail.com>
2026-02-17 09:20:16 +0530

    Coded to print oops banner

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,31 @@
+public class OopsBannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS Banner App");
+        System.out.println("UC2: Render OOPS as Banner using Print Statements");
+        System.out.println();
+
+        // Line 1
+        System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
+
+        // Line 2
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+
+        // Line 3
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+
+        // Line 4
+        System.out.println(" *     *  " + " *     *  " + " ******   " + "  *****   ");
+
+        // Line 5
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+
+        // Line 6
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+
+        // Line 7
+        System.out.println("  *****   " + "  *****   " + " *        " + " ******   ");
+    }
+}
+
