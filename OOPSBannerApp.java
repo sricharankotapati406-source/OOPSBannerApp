7e01ae84645b3edf0b463505d7e6b170bb262add
sri charan <sricharankotapati406@gmail.com>
2026-03-02 08:02:46 +0530

    UC3: Refracter banner using string.join()

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * This class demonstrates a simple Java application
+ * that displays the literal text "OOPS".
+ *
+ * @author Sricharan
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
